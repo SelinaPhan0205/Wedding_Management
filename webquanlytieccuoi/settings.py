b15e7f0c3473a3756b4fc6be3256f521627b0de9
TheDang0502 <dangtran09234@gmail.com>
2025-07-10 11:47:02 +0700

    Sửa nút xóa

--- webquanlytieccuoi/settings.py
+++ webquanlytieccuoi/settings.py
@@ -64,7 +64,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "Wedding_Management",
         "USER": "postgres",
-        "PASSWORD": "8436530leo",
+        "PASSWORD": "050225",
         "HOST": "localhost",
         "PORT": "5432",  # Thêm cổng mặc định của PostgreSQL
     }
